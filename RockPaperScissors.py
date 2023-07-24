386d2152cb93ee9f00726b10089b8432d48cc49e
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2023-07-24 10:26:12 +0100

    feat: Add random computer choice generation.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -3,3 +3,12 @@ import random               # using it to generate computer choices
 while True:
     #user input
     user_action = input("Enter a choice (rock, paper, scissors): ") 
+    
+    # computer chooses 
+    possible_actions = ["rock", "paper", "scissors"]
+    
+    #computer action 
+    computer_action = random.choice(possible_actions)   #computer selects a random  action using random.choice 
+
+    # print the choices that the user and the computer made:
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
