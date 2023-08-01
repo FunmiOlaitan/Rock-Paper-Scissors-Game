82217f28e0352f996c001b9de8a4e5962adcb57a
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2023-08-01 22:35:24 +0100

    feat: Implement logic to determine the winner

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -12,3 +12,22 @@ while True:
 
     # print the choices that the user and the computer made:
     print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+     #Determine a Winner
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!") #in the event that its a tie and user & computer choose paper 
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
